a30d1b08e968b345a0c3bd56332304d9dc176285
serxa <sergiose2010@gmail.com>
2018-11-29 09:53:44 +0100

    Comentarios añadidos

--- Ejercicio_3/src/threads3/Threads3.java
+++ Ejercicio_3/src/threads3/Threads3.java
@@ -1,3 +1,11 @@
+/*
+Realizar un programa que cree un fío, que a súa vez cree outro fío, e así consecutivamente,
+ata un total de cinco fíos. Cada fío debe presentar o seu nome indicando que comenzou, crear
+o seu fío fillo e presentar o seu nome 10 veces indicando que se está procesando, esperando
+un tempo aleatorio entre 100 e 600 milisegundos entre cada presentación. Cada fío deberá
+esperar a que o seu fillo termine antes de presentar o seu último mensaxe indicando o seu
+nome e que rematou
+*/
 package threads3;
 
 import java.util.logging.Level;
@@ -14,7 +22,9 @@ public class Threads3 extends Thread{
     public void run(){
         try{
             System.out.println("El "+getName() +" ha comenzado");
-            
+            /**
+             * Bucle que crea los thread y los inicia
+             */
             while(count <= 5){
                 count ++;
                 
@@ -22,9 +32,11 @@ public class Threads3 extends Thread{
                 thread.start();
                 
                 for(int i=0;i<10;i++){
+                    //hacemos que el thread escriba algo y se duerma
                     System.out.println(getName()+ " en proceso");
                     sleep((long) ((Math.random() * (600 - 100)) + 100));
                 }
+                //sincronizamos
                 thread.join();
                 System.out.println("El "+ getName()+" ha terminado");
             }
