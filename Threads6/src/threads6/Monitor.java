178bf30d94862a4a36a8c6023172102760657628
serxa <sergiose2010@gmail.com>
2018-11-29 09:52:18 +0100

    Comentarios añadidos

--- Threads6/src/threads6/Monitor.java
+++ Threads6/src/threads6/Monitor.java
@@ -1,14 +1,74 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package threads6;
 
-/**
- *
- * @author serxa
- */
-public class Monitor {
-    
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Monitor extends Thread {
+
+    int dinero = 10;
+    String nombre;
+    boolean ingreso=false;
+    boolean retirada=false;
+
+    public Monitor(String nombre) {
+        super(nombre);
+        this.nombre = nombre;
+    }
+
+    public synchronized void ingresar() throws InterruptedException {
+        while(retirada==true){
+            wait();
+        }
+        ingreso=true;
+        System.out.println(getName() + " va a realizar un ingreso, dinero actual: " + dinero);
+        dinero++;
+        System.out.println(nombre + " total: " + dinero + "\n");
+        ingreso=false;
+        notify();
+    }
+
+    public synchronized void retirar() throws InterruptedException {
+        while(ingreso==true){
+            wait();
+        }
+        retirada=true;
+        System.out.println(getName() + " va a realizar una retirada, dinero actual: " + dinero);
+        if (dinero <= 0) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                System.out.println("Error: " + e);
+            }
+        }
+
+        dinero--;
+        System.out.println(nombre + " total: " + dinero + "\n");
+        retirada=false;
+        notify();
+
+    }
+
+    @Override
+    public void run() {
+        if ("Consumidor".equalsIgnoreCase(nombre)) {
+            for (int i = 0; i < 5; i++) {
+                try {
+                    retirar();
+                } catch (InterruptedException ex) {
+                    System.out.println("Error: "+ex);
+                }
+            }
+        }
+
+        if ("Productor".equalsIgnoreCase(nombre)) {
+            for (int i = 0; i < 5; i++) {
+                try {
+                    ingresar();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Monitor.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+
 }
--- Threads6/src/threads6/main.java
+++ Threads6/src/threads6/main.java
@@ -3,7 +3,11 @@ package threads6;
 public class main {
 
     public static void main(String[] args) {
+        Monitor p = new Monitor("Productor");
+        Monitor c = new Monitor("Consumidor");
         
+        p.start();
+        c.start();
     }
-    
+
 }
