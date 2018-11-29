558a3195868b97e92538fbd2aeb1c104aa44bd33
serxa <sergiose2010@gmail.com>
2018-11-29 09:53:48 +0100

    Comentarios añadidos

--- Ejercicio_2/src/threads2/Threads2.java
+++ Ejercicio_2/src/threads2/Threads2.java
@@ -1,3 +1,9 @@
+/*
+Programa que crea 4 fíos, os executa e espera a que estes terminen. Os fíos escriben 5 veces o
+número de iteración do bucle e o seu nome. En cada iteración, despois de escribir o seu nome,
+bloquéanse durante un segundo e volven a estar dispoñibles para a súa execución.
+*/
+
 package threads2;
 
 public class Threads2 extends Thread {
@@ -5,11 +11,15 @@ public Threads2 (String nombre){
     super(nombre);
 }
 
+/**
+ * Execucion dos thread
+ */  
 @Override
 public void run(){
     for(int i=0; i<5; i++){
         System.out.println(i+" "+getName());
         try{
+            //dormimos o thread
             Thread.sleep(1000);
         }catch(InterruptedException ex){
             System.out.println("Error: "+ex);
@@ -19,6 +29,7 @@ public void run(){
     System.out.println("Termina thread "+getName());
 }
     public static void main(String[] args) {
+        //iniciamos os threads
         new Threads2("Pepe").start();
         new Threads2("Tomas").start();
         new Threads2("Andre").start();
